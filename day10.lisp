220dbb94d6d157589dc65691ef9e0d9e52c178e7
serdar <serdar@cnkamsterdam.com>
2023-12-31 16:30:55 +0100

    Day10 prettify and part2 first attempt

--- day10.lisp
+++ day10.lisp
@@ -8,6 +8,67 @@
 (defvar *output-stream* t)
 
 
+(defun create-node(location &key char west east north south is-start)
+  (let ((_location location)
+        (_is-start is-start)
+        (_char char)
+        (_west west)
+        (_east east)
+        (_north north)
+        (_south south))
+    (let ((node (list
+                 _location
+                 _is-start
+                 #'(lambda (&key operation value) (if (eql operation :get) _char (setf _char value)))
+                 #'(lambda (&key operation value) (if (eql operation :get) _west (setf _west value)))
+                 #'(lambda (&key operation value) (if (eql operation :get) _east (setf _east value)))
+                 #'(lambda (&key operation value) (if (eql operation :get) _north (setf _north value)))
+                 #'(lambda (&key operation value) (if (eql operation :get) _south (setf _south value))))))
+      (if *nodes*
+          (setf (gethash _location *nodes*) node)
+          node))))
+
+(defun node-get-location(node)
+  (car node))
+
+(defun node-is-starting-node(node)
+  (not (null (cadr node))))
+
+(defmacro node-get-char (node)
+  `(funcall (third ,node) :operation :get))
+(defmacro node-set-char (node value)
+  `(funcall (third ,node) :operation :set :value ,value))
+
+(defmacro node-get-west (node)
+  `(funcall (fourth ,node) :operation :get))
+(defmacro node-set-west (node value)
+  `(funcall (fourth ,node) :operation :set :value ,value))
+
+(defmacro node-get-east (node)
+  `(funcall (fifth ,node) :operation :get))
+(defmacro node-set-east (node value)
+  `(funcall (fifth ,node) :operation :set :value ,value))
+
+(defmacro node-get-north (node)
+  `(funcall (sixth ,node) :operation :get))
+(defmacro node-set-north (node value)
+  `(funcall (sixth ,node) :operation :set :value ,value))
+
+(defmacro node-get-south (node)
+  `(funcall (seventh ,node) :operation :get))
+(defmacro node-set-south (node value)
+  `(funcall (seventh ,node) :operation :set :value ,value))
+
+
+(defun node-to-string(node &optional (prefix "Node: "))
+  (if node
+      (format *output-stream* "~a ~a ~a \"~a\" ~%"
+              prefix
+              (node-get-location node)
+              (node-is-starting-node node)
+              (node-get-char node))
+      (format *output-stream* "~A nil~%" prefix)))
+
 (defun day10-load(&key load-test)
   (let ((rows nil)
         (row-count 0))
@@ -32,37 +93,9 @@
                  do (when (char= #\S (aref *maze-data* i j))
                       (return-from find-starting-location (list i j))))))
 
-(defun create-node(location &key char west east north south is-start)
-  (let ((_location location)
-        (_is-start is-start)
-        (_char char)
-        (_west west)
-        (_east east)
-        (_north north)
-        (_south south))
-    (let ((node (list
-                 _location
-                 _is-start
-                 #'(lambda (&key operation value) (if (eql operation :get) _char (setf _char value)))
-                 #'(lambda (&key operation value) (if (eql operation :get) _west (setf _west value)))
-                 #'(lambda (&key operation value) (if (eql operation :get) _east (setf _east value)))
-                 #'(lambda (&key operation value) (if (eql operation :get) _north (setf _north value)))
-                 #'(lambda (&key operation value) (if (eql operation :get) _south (setf _south value))))))
-      (if *nodes*
-          (setf (gethash _location *nodes*) node)
-          node))))
-
-(defun node-to-string(node &optional (prefix "Node: "))
-  (if node
-      (destructuring-bind (location is-start fn-char w e n s)
-          node
-        (format *output-stream* "~a ~a ~a \"~a\" ~%" prefix location is-start (funcall fn-char :operation :get)))
-      (format *output-stream* "~A nil~%" prefix)))
-
-
-(defun build-graph (starting-node)
-  (destructuring-bind ((row column) is-start fn-char fn-west fn-east fn-north fn-south)
-      starting-node
+(defun prepare-node (node)
+  (destructuring-bind (row column)
+      (node-get-location node)
     (let ((west-char (when (> column 0) (aref *maze-data* row (1- column))))
           (east-char (when (< column (1- (cadr *maze-dimensions*))) (aref *maze-data* row (1+ column))))
           (north-char (when (> row 0) (aref *maze-data* (1- row) column)))
@@ -71,52 +104,38 @@
           (east-node (gethash (list row (1+ column)) *nodes*))
           (north-node (gethash (list (1- row) column) *nodes*))
           (south-node (gethash (list (1+ row) column) *nodes*)))
-      ;; (node-to-string starting-node)
+
+      (when (node-is-starting-node node)
+        nil) ;; TODO: fix the character of starting node
 
       (if (and west-char
-               (null (funcall fn-west :operation :get))
-               (position (funcall fn-char :operation :get) "-7J" :test #'char=)
+               (null (node-get-west node))
+               (position (node-get-char node) "-7J" :test #'char=)
                (position west-char "-FL" :test #'char=))
-          (progn
-            ;; (node-to-string west-node "Child West: ")
-            (funcall fn-west
-                     :operation :set
-                     :value west-node))
+          (node-set-west node west-node)
           (setf west-node nil))
 
       (if (and east-char
-               (null (funcall fn-east :operation :get))
-               (position (funcall fn-char :operation :get) "-FL" :test #'char=)
+               (null (node-get-east node))
+               (position (node-get-char node) "-FL" :test #'char=)
                (position east-char "-7J" :test #'char=))
-          (progn
-            ;; (node-to-string east-node "Child East: ")
-            (funcall fn-east
-                     :operation :set
-                     :value east-node))
+          (node-set-east node east-node)
           (setf east-node nil))
 
       (if (and north-char
-               (null (funcall fn-north :operation :get))
-               (position (funcall fn-char :operation :get) "|JL" :test #'char=)
+               (null (node-get-north node))
+               (position (node-get-char node) "|JL" :test #'char=)
                (position north-char "|7F" :test #'char=))
-          (progn
-            ;; (node-to-string north-node "Child North: ")
-            (funcall fn-north
-                     :operation :set
-                     :value north-node))
+          (node-set-north node north-node)
           (setf north-node nil))
 
       (if (and south-char
-               (null (funcall fn-south :operation :get))
-               (position (funcall fn-char :operation :get) "|7F" :test #'char=)
+               (null (node-get-south node))
+               (position (node-get-char node) "|7F" :test #'char=)
                (position south-char "|JL" :test #'char=))
-          (progn
-            ;; (node-to-string south-node "Child South: ")
-            (funcall fn-south
-                     :operation :set
-                     :value south-node))
+          (node-set-south node south-node)
           (setf south-node nil))))
-  starting-node)
+  node)
 
 (defun create-graph(&key (starting-pipe #\S))
   (let ((*nodes* (make-hash-table :test #'equal)))
@@ -124,8 +143,9 @@
     (loop for row from 0 to (1- (car *maze-dimensions*))
           do (loop for column from 0 to (1- (cadr *maze-dimensions*))
                    do (let ((_node (gethash (list row column) *nodes*)))
-                        (build-graph _node))))
-    *nodes*))
+                        (prepare-node _node))))
+    (let ((starting-location (find-starting-location)))
+      (gethash starting-location *nodes*))))
 
 (defun create-all-nodes(&key (starting-pipe #\S))
   (loop for row from 0 to (1- (car *maze-dimensions*))
@@ -135,21 +155,22 @@
                                   :char (if (char= #\S pipe) starting-pipe pipe)
                                   :is-start (char= #\S pipe))))))
 
-(defun traverse-graph (node fn &key initial-run skip)
+(defun traverse-graph (node fn-action &key initial-run skip)
   (when node
-    (destructuring-bind (location is-start fn-char fn-west fn-east fn-north fn-south)
-        node
-      (when (or (null is-start) initial-run)
-        (funcall fn node)
-        (cond ((and (not (eql skip :west)) (funcall fn-west :operation :get))
-               (traverse-graph (funcall fn-west :operation :get) fn :skip :east))
-              ((and (not (eql skip :east)) (funcall fn-east :operation :get))
-               (traverse-graph (funcall fn-east :operation :get) fn :skip :west))
-              ((and (not (eql skip :north)) (funcall fn-north :operation :get))
-               (traverse-graph (funcall fn-north :operation :get) fn :skip :south))
-              ((and (not (eql skip :south)) (funcall fn-south :operation :get))
-               (traverse-graph (funcall fn-south :operation :get) fn :skip :north)))))))
-
+    (when (or (not (node-is-starting-node node)) initial-run)
+      (let ((west (node-get-west node))
+            (east (node-get-east node))
+            (north (node-get-north node))
+            (south (node-get-south node)))
+        (funcall fn-action node)
+        (cond ((and (not (eql skip :west)) west)
+               (traverse-graph west fn-action :skip :east))
+              ((and (not (eql skip :east)) east)
+               (traverse-graph east fn-action :skip :west))
+              ((and (not (eql skip :north)) north)
+               (traverse-graph north fn-action :skip :south))
+              ((and (not (eql skip :south)) south)
+               (traverse-graph south fn-action :skip :north)))))))
 
 (defun day10-part1 (&key is-test)
   (let* ((*maze-data* (day10-load :load-test is-test))
@@ -159,50 +180,47 @@
                                       :direction :output
                                       :if-exists :supersede
                                       :if-does-not-exist :create)
-      (let ((starting-location (find-starting-location))
-            (graph (create-graph :starting-pipe (if is-test #\F #\|))))
-        (traverse-graph (gethash starting-location graph)
+      (let ((graph (create-graph :starting-pipe (if is-test #\F #\|)))) ;; TODO: fix starting pipe
+        (traverse-graph graph
                         #'(lambda(n)
-                            (format *output-stream* "~A: ~A~%" (car n) (funcall (caddr n) :operation :get)))
+                            (format *output-stream* "~A: ~A~%" (node-get-location n) (node-get-char n)))
                         :initial-run t)
         graph))))
 
 
 
 (defun day10-part2 (&key is-test)
-  (let ((*maze-data* (day10-load :load-test is-test))
-        (*maze-dimensions* (array-dimensions *maze-data*)))
-    (with-open-file (*output-stream*  #P"output10-part2.txt"
+  (let* ((*maze-data* (day10-load :load-test is-test))
+         (*maze-dimensions* (array-dimensions *maze-data*)))
+    (with-open-file (*output-stream*  #P"output10.txt"
                                       :direction :output
                                       :if-exists :supersede
                                       :if-does-not-exist :create)
-      (let ((new-maze (make-array '(140 140) :element-type 'character :initial-element #\.)))
-        (traverse-graph (gethash '(60 75) (create-graph))
+      (let ((new-maze (make-array *maze-dimensions* :element-type 'character :initial-element #\.))
+            (graph (create-graph :starting-pipe (if is-test #\F #\|)))) ;; TODO: fix starting pipe
+        (traverse-graph graph
                         #'(lambda(n)
-                            (setf (aref new-maze (caar n) (cadar n)) #\X))
+                            (setf (aref new-maze (caar n) (cadar n)) (node-get-char n)))
                         :initial-run t)
-        (loop for row from 0 to 139
-              do (loop for column from 0 to 139
+        (loop for row from 0 to (1- (car *maze-dimensions*))
+              do (loop for column from 0 to (1- (cadr *maze-dimensions*))
                        do (progn
                             (format *output-stream* "~a" (aref new-maze row column))
-                            (when (= column 139)
+                            (when (= column (1- (cadr *maze-dimensions*)))
                               (format *output-stream* "~%")))))))))
 
 
-
-
-
-
-(defun day10-part2()
+#||(defun day10-part2-find()
 (with-open-file (*output-stream* #P"output10-part2.txt"
-                                 :direction :output
-                                 :if-exists :supersede
-                                 :if-does-not-exist :create)
-  (let ((new-maze (make-array '(140 140) )))
-    (traverse-graph (gethash '(60 75) (create-graph))
-                    #'(lambda(n)
-                        (format aoc::*output-stream*
-                                "~A: ~A~%"
-                                (car n)
-                                (funcall (caddr n) :operation :get)))
-                    :initial-run t))))
+:direction :output
+:if-exists :supersede
+:if-does-not-exist :create)
+(let ((new-maze (make-array '(140 140) )))
+(traverse-graph (gethash '(60 75) (create-graph))
+#'(lambda(n)
+(format aoc::*output-stream*
+"~A: ~A~%"
+(car n)
+(funcall (caddr n) :operation :get)))
+:initial-run t))))
+|#
